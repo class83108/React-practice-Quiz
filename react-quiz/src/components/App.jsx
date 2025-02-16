d315544a526b7dfa4ea4631e69a9f1a8c36e3b29
Eason_chiang <95297596+class83108@users.noreply.github.com>
2025-02-16 20:33:17 +0800

    重構 React Quiz 應用程式，新增組件並移除不必要的檔案

--- react-quiz/src/components/App.jsx
+++ react-quiz/src/components/App.jsx
@@ -12,6 +12,9 @@ const initialState = {
 
   // loading, error, ready, active, finished
   status: "loading",
+
+  // 用來判斷現在要顯示的問題
+  index: 0,
 };
 
 function reducer(state, action) {
@@ -28,7 +31,10 @@ function reducer(state, action) {
 }
 
 export default function App() {
-  const [{ questions, status }, dispatch] = useReducer(reducer, initialState);
+  const [{ questions, status, index }, dispatch] = useReducer(
+    reducer,
+    initialState
+  );
 
   const numQuestions = questions.length;
 
@@ -57,7 +63,7 @@ export default function App() {
         {status === "ready" && (
           <StartScreen numQuestions={numQuestions} dispatch={dispatch} />
         )}
-        {status === "active" && <Question />}
+        {status === "active" && <Question question={questions[index]} />}
       </MainContent>
     </div>
   );
